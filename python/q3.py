4fa0f06b7497b3d07f6bc96f1adeecbe259a90fc
Senjuti Saha <113691557+shuamamine@users.noreply.github.com>
2023-09-08 03:24:02 +0530

    Update q3.py

--- python/q3.py
+++ python/q3.py
@@ -10,15 +10,15 @@ for i in range(0,n):
 z=0
 for i in range(0,n):
     for j in range(0,n):
-        print(arr[i][j]," ",end="")
+        print(a[i][j]," ",end="")
     print("\n")
 for i in range(0,n):
     l=[]
     for j in range(0,n):
-        l.append(arr[(n-1)-j][i])
-    arr1.append(l)
+        l.append(a[(n-1)-j][i])
+    ar.append(l)
 print("Rotated Matrix:")
 for i in range(0,n):
     for j in range(0,n):
-        print(arr1[i][j]," ",end="")
+        print(ar[i][j]," ",end="")
     print("\n")
