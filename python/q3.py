33c4208f324a7a9a86f7f3a25972e7dc40e85502
Senjuti Saha <113691557+shuamamine@users.noreply.github.com>
2023-09-07 23:59:16 +0530

    Create q3.py

---
+++ python/q3.py
@@ -0,0 +1,24 @@
+n=int(input("Enter the size of the matrix:"))
+arr=[]
+arr1=[]
+for i in range(0,n):
+    l=[]
+    for j in range(0,n):
+        num=int(input("Enter the elements:"))
+        l.append(num)
+    arr.append(l)
+z=0
+for i in range(0,n):
+    for j in range(0,n):
+        print(arr[i][j]," ",end="")
+    print("\n")
+for i in range(0,n):
+    l=[]
+    for j in range(0,n):
+        l.append(arr[(n-1)-j][i])
+    arr1.append(l)
+print("Roatated Matrix:")
+for i in range(0,n):
+    for j in range(0,n):
+        print(arr1[i][j]," ",end="")
+    print("\n")
